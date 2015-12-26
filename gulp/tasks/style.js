03138779486f6b54d6624aca963952449440ebfa
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-12-27 00:33:12 +0900

    Change css minify操作はbuild時にuserefと行うようにしたため、devとbuildでpostCssのpluginsを分ける必要がなくなった

--- gulp/tasks/style.js
+++ gulp/tasks/style.js
@@ -7,38 +7,27 @@ const rename = require('gulp-rename');
 
 const browserSync = require('browser-sync');
 
-const postCssPlugins = {
-  dev: [
-    require('doiuse')(config.doiuse.options),
-    require('autoprefixer')(config.autoprefixer.options),
-    require('cssfmt'),
-    require('stylelint')(config.stylelint.options),
-    require('postcss-reporter')(config['postcss-reporter'].options),
-  ],
-  build: [
-    require('doiuse')(config.doiuse.options),
-    require('autoprefixer')(config.autoprefixer.options),
-    require('stylelint')(config.stylelint.options),
-    require('csswring')(config.csswring.options),
-    require('postcss-reporter')(config['postcss-reporter'].options),
-  ],
-};
+const postCssPlugins = [
+  require('doiuse')(config.doiuse.options),
+  require('autoprefixer')(config.autoprefixer.options),
+  require('cssfmt'),
+  require('stylelint')(config.stylelint.options),
+  require('postcss-reporter')(config['postcss-reporter'].options),
+];
 
 gulp.task('style:dev', () => gulp.src(config.paths.css.src)
-    .pipe(plumber())
-    .pipe(postcss(postCssPlugins.dev))
-    .pipe(rename({
-      extname: '.css',
-    }))
-    .pipe(gulp.dest(`${config.rootDirs.tmp}`))
-    .pipe(browserSync.get(config.browserSync.namespace.dev).stream()));
+  .pipe(plumber())
+  .pipe(postcss(postCssPlugins))
+  .pipe(rename({
+    extname: '.css',
+  }))
+  .pipe(gulp.dest(`${config.rootDirs.tmp}`))
+  .pipe(browserSync.get(config.browserSync.namespace.dev).stream()));
 
 gulp.task('style:build', () => gulp.src(config.paths.css.src)
-    .pipe(plumber())
-    .pipe(postcss(postCssPlugins.build))
-
-    // TODO ここでHeaderを追加
-    .pipe(rename({
-      extname: '.css',
-    }))
-    .pipe(gulp.dest(`${config.rootDirs.dist}`)));
+  .pipe(plumber())
+  .pipe(postcss(postCssPlugins))
+  .pipe(rename({
+    extname: '.css',
+  }))
+  .pipe(gulp.dest(`${config.rootDirs.tmp}`)));
