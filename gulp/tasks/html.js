1e89dde52fb3ae873b900b452cc99dc3a9f758a8
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-12-27 00:33:40 +0900

    Add html用のbuildタスクを追加した

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,10 +1,10 @@
-const gulp = require('gulp');
-
 const config = require('../config');
+
+const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const htmlhint = require('gulp-htmlhint');
 
-gulp.task('html', () => {
+gulp.task('html:dev', () => {
   gulp.src(config.paths.html.src)
     .pipe(plumber())
     .pipe(htmlhint())
@@ -12,4 +12,13 @@ gulp.task('html', () => {
     .pipe(htmlhint.failReporter())
     .pipe(gulp.dest(`${config.rootDirs.tmp}`))
     .on('end', () => require('browser-sync').get(config.namespace.dev).reload);
+});
+
+gulp.task('html:build', () => {
+  gulp.src(config.paths.html.src)
+    .pipe(plumber())
+    .pipe(htmlhint())
+    .pipe(htmlhint.reporter())
+    .pipe(htmlhint.failReporter())
+    .pipe(gulp.dest(`${config.rootDirs.tmp}`));
 });
